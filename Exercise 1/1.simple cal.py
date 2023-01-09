386ec22f713bd5280606e1d592cf7e9cf74326ce
axaish <112846324+axaish@users.noreply.github.com>
2023-01-09 14:23:45 +0530

    Add files via upload

---
+++ Exercise 1/1.simple cal.py
@@ -0,0 +1,12 @@
+m=int(input("Enter 1st int:"))
+n=int(input("Enter 2nd int:"))
+def cal():
+    s=m+n
+    sub=m-n
+    mul=m*n
+    div=m/n
+    print("Addition:",s)
+    print("Subtraction:",sub)
+    print("Multiplication:",mul)
+    print("Division:",div)
+print("cal:",cal())
